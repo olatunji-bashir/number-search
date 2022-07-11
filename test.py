58bc90483c89a862ee280920e323874d91d8db16
olatunji-bashir <bashir.alokun@yahoo.com>
2022-07-11 13:45:23 +0100

    initail commit

---
+++ test.py
@@ -0,0 +1,30 @@
+from jovian.pythondsa import evaluate_test_case
+
+
+test = {
+    'input': {'cards': [13, 11, 10, 7, 4, 3, 1, 0],
+    'query':7
+    },
+    'output':3
+}
+
+
+
+print(evaluate_test_case.test) 
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
