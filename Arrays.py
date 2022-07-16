4c461ddf8c2741127f1dc8d084a843b70daeca1c
olatunji-bashir <109037675+olatunji-bashir@users.noreply.github.com>
2022-07-16 08:32:11 +0100

    Add files via upload
    Learnt what an array mean list in python

---
+++ Arrays.py
@@ -0,0 +1,29 @@
+new_list = [1, 2, 3]
+result = new_list[0]
+
+if 1 in new_list: print(True)
+
+for n in new_list:
+    if n == 1:
+        print(True)
+
+        break
+
+
+
+numbers = []
+print(len(numbers))
+
+numbers.append(2)
+numbers.append(200)
+
+print(len(numbers))
+
+numbers.extend([4, 5, 6])
+
+print(numbers)
+
+
+
+
+
