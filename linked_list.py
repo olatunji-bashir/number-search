f8af2a5f43e6de54ae602012bdf704494fc0c489
olatunji-bashir <109037675+olatunji-bashir@users.noreply.github.com>
2022-07-16 13:12:19 +0100

    Add files via upload
    Implemented more functions to the linked list

--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,7 @@
+from ast import Return
+from turtle import position
+
+
 class Node:
     """
     An object for storing a single node of a linked list.
@@ -42,6 +46,105 @@ class LinkedList:
 
         return count
 
+    def add(self, data):
+        """
+        Adds a new Node containing data at head of the list
+        Takes 0(1) time
+        """
+        
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def search(self, key):
+        """
+        Search for the first node containing data that matches the key
+        return the node or 'None' id not found
+
+        Takes 0(n) time
+        """
+        
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+    def insert(self, data, index):
+        """
+        Inserts a new node containing data at index position
+        Insertion tkes 0(1) time but finding the node at the 
+        insertion point takes 0(n) time.
+
+        Takes overall 0(n) time
+        """
+        if index == 0:
+            self.add(data)
+        
+        if index > 0:
+            new = Node(data)
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = Node.next_node
+                position -= 1
+            
+            prev_node = current
+            next_node = current.next_node
+
+            prev_node.next_node = new
+            new.next_node = next_node
+
+    def remove(self, key):
+        """
+        Removes Node containing data that matches the key
+        Returns the node or None if the key doesn't exist
+        Takes 0(n) time 
+        """
+        
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+        
+        return current
+
+
+
+    def __repr__(self):
+        """
+        Return a string representation of the list.
+        Takes O(n) time.
+        """
+        
+        nodes = []
+        current = self.head
+        
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+            
+            current = current.next_node
+        
+        return  '-> '.join(nodes)
 
 
 
@@ -52,6 +155,45 @@ N2 = Node(20)
 N1.next_node = N2
 
 print(N1.next_node)
+
+
+l = LinkedList()
+N1 = Node(10)
+l.head = N1
+print(l.size())
+
+
+l = LinkedList()
+l.add(1)
+print(l.size())
+
+l.add(2)
+l.add(3)
+print(l.size())
+
+print(l)
+
+print(l.search(1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
