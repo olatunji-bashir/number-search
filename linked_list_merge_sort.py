4cc0e0ae6c61844e3edeb2f226a54f83231dd8f3
olatunji-bashir <109037675+olatunji-bashir@users.noreply.github.com>
2022-07-17 21:15:07 +0100

    Add files via upload
    updated the merge function

--- linked_list_merge_sort.py
+++ linked_list_merge_sort.py
@@ -1,5 +1,3 @@
-from heapq import merge
-from turtle import left
 from linked_list import LinkedList
 
 def merge_sort(linked_list):
@@ -17,8 +15,6 @@ def merge_sort(linked_list):
     elif linked_list.head is None:
         return linked_list
 
-
-
     left_half, right_half = split(linked_list)
     left = merge_sort(left_half)
     right = merge_sort(right_half)
@@ -35,15 +31,13 @@ def split(linked_list):
         right_half = None
 
         return left_half, right_half
-    
     else:
         size = linked_list.size()
         mid = size//2
-
         mid_node = linked_list.node_at_index(mid-1)
 
         left_half = linked_list
-        right_half = linked_list()
+        right_half = LinkedList()
         right_half.head = mid_node.next_node
         mid_node.next_node = None
 
@@ -61,11 +55,64 @@ def merge(left, right):
     merged = LinkedList()
 
     # add a fake head that is discarded later
+    merged.add(0)
+
+    # Set current to the head of the linked list
+    current = merged.head
+
+    # obtain head nodes for left and right linked lists
+    left_head = left.head
+    right_head = right.head
+
+    # Iterate over left and right until we reach the tail node
+    # of either
+    while left_head or right_head:
+        # if the head node of left is None, we're past the tail
+        # Add the node from right to merged linked list 
+        if left_head is None:
+            current.next_node = right_head
+            #call next on right to set loop condition to false
+            right_head = right_head.next_node
+        # if the head node of right is None, we're past the tail
+        # Add the tail node from left to merge linked list
+        elif right_head is None:
+            current.next_node = left_head
+            #call next on left to set loop condition to False
+            left_head = left_head.next_node
+        else:
+            # Not at either tail node
+            # Obtain node data to perform comparison operation
+            left_data = left_head.data
+            right_data = right_head.data
+            # If data on left is less than right, set current to left node
+            if left_data < right_data:
+                current.next_node = left_head
+                # Move left head to next node
+                left_head = left_head.next_node
+            # If data on left is greater than right, set current to right node
+            else:
+                current.next_node = right_head
+                right_head = right_head.next_node
+        # Move current to next node
+        current = current.next_node
+
+    # Discard fake head and set first merged node as head
+    head = merged.head.next_node
+    merged.head = head
+
+    return merged
 
 
+l = LinkedList()
+l.add(10)       
+l.add(2) 
+l.add(44) 
+l.add(15) 
+l.add(200) 
 
-
-
+print(l)
+sorted_linked_list = merge_sort(l)
+print(sorted_linked_list)
 
 
 
