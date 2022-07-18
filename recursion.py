2f7a199f79fa4122e78f2607aa1fb677ed2c8acb
olatunji-bashir <109037675+olatunji-bashir@users.noreply.github.com>
2022-07-18 21:07:08 +0100

    Add files via upload
    Recursion sort algorithm

---
+++ recursion.py
@@ -0,0 +1,17 @@
+import numbers
+
+
+def sum(numbers):
+    if not numbers:
+        return 0
+    print("Calling sum(%s)" % numbers[1:])
+    remaining_sum = sum(numbers[1:])
+    print("Call to sum(%s) returning %d + %d" % (numbers, numbers[0], remaining_sum))
+    return numbers[0] + remaining_sum
+
+
+
+print(sum([1, 2, 7, 9]))
+
+
+
